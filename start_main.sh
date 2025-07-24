37aee3a6fb27314aedd0437d1a4e8aa4f4c3f876
Corian (Cory) Kennedy <cory@darkintel.io>
2025-07-23 22:29:57 -0500

    Update start_main.sh

--- start_main.sh
+++ start_main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VENV_NAME="panhandlr_venv"
-echo "[+] PANHANDLR building enviornment and charging laz0rz"
+echo "[+] Welcome to Panhandlr Setup"
 
 if [ -d "$VENV_NAME" ]; then
+    echo "[*] Virtual environment '$VENV_NAME' already exists. Activating."
 else
+    echo "[*] Creating Python virtual environment: $VENV_NAME..."
     python3 -m venv "$VENV_NAME"
 fi
 
@@ -13,7 +15,8 @@ source "$VENV_NAME/bin/activate"
 pip install -q --upgrade pip
 pip install -q -r requirements.txt
 
+
 python3 panhandlr.py
 
 deactivate
-echo "[+] Panhandler exited."
+echo "[+] Panhandlr exited."
